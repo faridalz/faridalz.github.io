f8e467c716a3a664834a04c717dbfbbbc6695f91
Farid Shiraliyev <79901122+faridalz@users.noreply.github.com>
2021-03-28 17:40:50 +0400

    Delete bs4 directory

--- bs4/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $("#butt").blur(function (event) {
-        var screenwidth = window.innerWidth;
-        if (screenwidth<768) {
-            $("#bs-example-navbar-collapse-1").collapse('hide');
-        }
-    });
-
-});
