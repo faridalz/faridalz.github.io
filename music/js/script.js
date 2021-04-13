a6e2d73b31d5860679f8e04f08edf69b8bc34fa3
Farid Shiraliyev <79901122+faridalz@users.noreply.github.com>
2021-04-13 17:02:56 +0400

    Update script.js

--- music/js/script.js
+++ music/js/script.js
@@ -1,9 +1,1 @@
-$(function () {
-    $("#butt2").blur(function (event) {
-        var screenwidth = window.innerWidth;
-        if (screenwidth<768) {
-            $("#nav1").collapse('hide');
-        }
-    });
 
-});
